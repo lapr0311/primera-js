ac558aef91e5e5929b806afea9a573f514a846fc
Luis Alejandro Peña Rivas <luisrivas0311@gmail.com>
2023-04-27 14:32:27 -0400

    final primera entrega

---
+++ index.js
@@ -0,0 +1,223 @@
+// entrada al concesionario
+
+alert("hola bienvenido al concesionario jeep");
+
+customerName = prompt("me podrias indicar tu nombre?");
+
+alert("hola  " + customerName + " bienvenido tengo estos 4 modelos para mostrar : Renegade,Rubicon,Compass,Gladietor");
+const Iva = 0.19;
+
+function CalcularIva(precios) {
+    let resultado = Iva * precios;
+    return resultado;
+
+
+}
+
+
+function modelosJeep() {
+    let modelo = prompt("ingresa el modelo que deseas adquirir!");
+    let precios;
+    let color;
+    let cicloColor;
+    let venta;
+    let valorIva;
+    let total;
+    while (modelo != "esc") {
+        switch (modelo) {
+            case "Renegade":
+                alert("perfecto tu auto seleccionado es un Renegade");
+                precios = 14000000;
+                alert("el valor de tu auto sin IVA es " + precios);
+                while (cicloColor != "esc") {
+                    alert("tengo estos colores disponibles: verde,gris,rojo");
+                    cicloColor = prompt("que Color deseas?");
+
+                    if (cicloColor == "verde") {
+                        confirm("prefecto tu color elegido a sido el verde");
+                        color = cicloColor;
+                        cicloColor = "esc";
+                    }
+                    else if (cicloColor == "gris") {
+                        confirm("prefecto tu color elegido a sido el gris");
+                        color = cicloColor;
+                        cicloColor = "esc";
+
+
+                    }
+                    else if (cicloColor == "rojo") {
+                        confirm("prefecto tu color elegido a sido el rojo");
+                        color = cicloColor;
+                        cicloColor = "esc";
+
+                    }
+                    else {
+                        confirm("lo sentimos no tenemos ese color");
+
+
+                    }
+                }
+
+
+
+                valorIva = CalcularIva(precios);
+                total = valorIva + precios;
+                alert("el costo de su auto es: " + precios + " y el IVA es " + valorIva + " dando un tota de " + total);
+                venta = confirm("estas seguro que desea comprar el auto " + modelo + "el total de la compra es " + total + " en el color " + color + "?");
+                if (venta == true) {
+                    alert("excelente su compra a sido exitosa");
+                    modelo = "esc";
+
+                } else {
+                    alert("upps su venta no fue realizada");
+                    modelo = prompt("ingresa el modelo que deseas adquirir!");
+                }
+
+                break;
+            case "Rubicon":
+                alert("perfecto tu auto seleccionado es un Rubicon");
+                precios = 18000000;
+                alert("el valor de tu auto sin IVA es " + precios);
+                while (cicloColor != "esc") {
+                    alert("tengo estos colores disponibles: verde,gris,rojo");
+                    cicloColor = prompt("que Color deseas?");
+
+                    if (cicloColor == "verde") {
+                        confirm("prefecto tu color elegido a sido el verde");
+                        color = cicloColor;
+                        cicloColor = "esc";
+                    }
+                    else if (cicloColor == "gris") {
+                        confirm("prefecto tu color elegido a sido el gris");
+                        color = cicloColor;
+                        cicloColor = "esc";
+
+
+                    }
+                    else if (cicloColor == "rojo") {
+                        confirm("prefecto tu color elegido a sido el rojo");
+                        color = cicloColor;
+                        cicloColor = "esc";
+
+                    }
+                    else {
+                        confirm("lo sentimos no tenemos ese color");
+
+
+                    }
+                }
+
+                valorIva = CalcularIva(precios);
+                total = valorIva + precios;
+                alert("el costo de su auto es: " + precios + " y el IVA es " + valorIva + " dando un tota de " + total);
+                venta = confirm("estas seguro que desea comprar el auto " + modelo + "el total de la compra es " + total + " en el color " + color + "?");
+                if (venta == true) {
+                    alert("excelente su compra a sido exitosa");
+                    modelo = "esc";
+
+                } else {
+                    alert("upps su venta no fue realizada");
+                    modelo = prompt("ingresa el modelo que deseas adquirir!");
+                }
+                break;
+            case "Compass":
+                alert("perfecto tu auto seleccionado es un Compass");
+                precios = 23000000;
+                alert("el valor de tu auto sin IVA es " + precios);
+                while (cicloColor != "esc") {
+                    alert("tengo estos colores disponibles: verde,gris,rojo");
+                    cicloColor = prompt("que Color deseas?");
+
+                    if (cicloColor == "verde") {
+                        confirm("prefecto tu color elegido a sido el verde");
+                        color = cicloColor;
+                        cicloColor = "esc";
+                    }
+                    else if (cicloColor == "gris") {
+                        confirm("prefecto tu color elegido a sido el gris");
+                        color = cicloColor;
+                        cicloColor = "esc";
+
+
+                    }
+                    else if (cicloColor == "rojo") {
+                        confirm("prefecto tu color elegido a sido el rojo");
+                        color = cicloColor;
+                        cicloColor = "esc";
+
+                    }
+                    else {
+                        confirm("lo sentimos no tenemos ese color");
+
+
+                    }
+                }
+                valorIva = CalcularIva(precios);
+                total = valorIva + precios;
+                alert("el costo de su auto es: " + precios + " y el IVA es " + valorIva + " dando un tota de " + total);
+                venta = confirm("estas seguro que desea comprar el auto " + modelo + "el total de la compra es " + total + " en el color " + color + "?");;
+                if (venta == true) {
+                    alert("excelente su compra a sido exitosa");
+                    modelo = "esc";
+
+                } else {
+                    alert("upps su venta no fue realizada");
+                    modelo = prompt("ingresa el modelo que deseas adquirir!");
+                }
+                break;
+            case "Gladietor":
+                alert("perfecto tu auto seleccionado es un Gladietor");
+                precios = 52000000;
+                alert("el valor de tu auto sin IVA es " + precios);
+                while (cicloColor != "esc") {
+                    alert("tengo estos colores disponibles: verde,gris,rojo");
+                    cicloColor = prompt("que Color deseas?");
+
+                    if (cicloColor == "verde") {
+                        confirm("prefecto tu color elegido a sido el verde");
+                        color = cicloColor;
+                        cicloColor = "esc";
+                    }
+                    else if (cicloColor == "gris") {
+                        confirm("prefecto tu color elegido a sido el gris");
+                        color = cicloColor;
+                        cicloColor = "esc";
+
+
+                    }
+                    else if (cicloColor == "rojo") {
+                        confirm("prefecto tu color elegido a sido el rojo");
+                        color = cicloColor;
+                        cicloColor = "esc";
+
+                    }
+                    else {
+                        confirm("lo sentimos no tenemos ese color");
+
+
+                    }
+                }
+                valorIva = CalcularIva(precios);
+                total = valorIva + precios;
+                alert("el costo de su auto es: " + precios + " y el IVA es " + valorIva + " dando un tota de " + total);
+                venta = confirm("estas seguro que desea comprar el auto " + modelo + "el total de la compra es " + total + " en el color " + color + "?");
+                if (venta == true) {
+                    alert("excelente su compra a sido exitosa");
+                    modelo = "esc";
+
+                } else {
+                    alert("upps su venta no fue realizada");
+                    modelo = prompt("ingresa el modelo que deseas adquirir!");
+                }
+                break;
+
+            default:
+                alert("modelo no encontrado");
+                alert("estos son los modelos  " + customerName + "  : Renegade,Rubicon,Compass,Gladietor");
+                modelo = prompt("ingresa el modelo que deseas adquirir!");
+                break;
+        }
+    }
+}
+
+modelosJeep();
